6dbaeabe470944425b91fddebe79d273fa9d7d54
Devendravaraprasad R <devendravaraprasadr@gmail.com>
2025-07-21 23:15:51 +0530

    Changes

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from parser import extract_receipt_data
 from db import init_db, insert_receipt, fetch_all_receipts
 
 # Tesseract path (adjust as needed)
-pytesseract.pytesseract.tesseract_cmd = r"C:\Users\devevara\OneDrive - Magna\DevaProjects\Tesseract-OCR\\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\devevara\OneDrive - Magna\DevaProjects\8-byte\Tesseract-OCR\\tesseract.exe"
 
 # Streamlit setup
 st.set_page_config(page_title="Receipt OCR App", layout="wide")
